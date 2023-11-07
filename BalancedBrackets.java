52e83b99e3716abb4f720cb8545dcc78808c8536
subhadra1990 <144151224+subhadra1990@users.noreply.github.com>
2023-11-07 20:08:23 +0530

    Add files via upload

---
+++ BalancedBrackets.java
@@ -0,0 +1,81 @@
+package balancedbrackets;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BalancedBrackets {
+
+	
+		         // function to check if brackets are balanced
+				static boolean areBracketsBalanced(String expr)
+				{
+					// Using ArrayDeque is faster than using Stack class
+					Deque<Character> stack	
+						= new ArrayDeque<Character>();
+
+					// Traversing the Expression
+					for (int i = 0; i < expr.length(); i++) {
+						char x = expr.charAt(i);
+
+						if (x == '(' || x == '[' || x == '{') {
+							// Push the element in the stack
+							stack.push(x);
+							continue;
+						}
+						// If current character is not opening
+						// bracket, then it must be closing. So stack
+						// cannot be empty at this point.
+						if (stack.isEmpty())
+							return false;
+						char check;
+						switch (x) {
+						case ')':
+							check = stack.pop();
+							if (check == '{' || check == '[')
+								return false;
+							break;
+
+						case '}':
+							check = stack.pop();
+							if (check == '(' || check == '[')
+								return false;
+							break;
+
+						case ']':
+							check = stack.pop();
+							if (check == '(' || check == '{')
+								return false;
+							break;
+						}
+					}
+
+					// Check Empty Stack
+					return (stack.isEmpty());
+				}
+
+				// Driver code
+				public static void main(String[] args)
+				{
+					String expr = "([[{}]])";
+		            
+					// Function call
+					if (areBracketsBalanced(expr))
+						System.out.println("The entered String"+ expr +" has Balanced Brackets ");
+					else
+						System.out.println("The entered Strings"+expr+" do not contain Balanced Brackets ");
+		            
+					String expr1 = "([[{}]]))";
+		            // Function call
+					if (areBracketsBalanced(expr1))
+						System.out.println("The entered String"+expr1+" has Balanced Brackets ");
+					else
+						System.out.println("The entered Strings"+expr1+" do not contain Balanced Brackets ");
+				}
+			
+				
+  }	
+
+
+	
+
+
